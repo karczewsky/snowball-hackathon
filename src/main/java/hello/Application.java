a5d6d82f172ef4c72f918eb2390c8847cfb0f238
Jakub Karczewski <jakub.karczewski@allegro.pl>
2021-12-13 13:30:24 +0100

    Update

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,94 +1,152 @@
 package hello;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.ServiceOptions;
+import com.google.cloud.bigquery.storage.v1.*;
+import com.google.protobuf.Descriptors;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.time.Instant;
 import java.util.List;
 import java.util.Map;
 
 @SpringBootApplication
 @RestController
 public class Application {
+    final String projectId = ServiceOptions.getDefaultProjectId();
+    final String datasetName = "snowball";
+    final String tableName = "events";
+    final WriteCommittedStream writeCommittedStream;
+    private Integer prevScore = -100;
+    private Integer counter = 0;
 
-  static class Self {
-    public String href;
-  }
-
-  static class Links {
-    public Self self;
-  }
-
-  static class PlayerState {
-    public Integer x;
-    public Integer y;
-    public String direction;
-    public Boolean wasHit;
-    public Integer score;
-  }
-
-  static class Arena {
-    public List<Integer> dims;
-    public Map<String, PlayerState> state;
-  }
-
-  static class ArenaUpdate {
-    public Links _links;
-    public Arena arena;
-  }
-
-  public static void main(String[] args) {
-    SpringApplication.run(Application.class, args);
-  }
-
-  @InitBinder
-  public void initBinder(WebDataBinder binder) {
-    binder.initDirectFieldAccess();
-  }
-
-  @GetMapping("/")
-  public String index() {
-    return "Let the battle begin!";
-  }
-
-  @PostMapping("/**")
-  public String index(@RequestBody ArenaUpdate arenaUpdate) {
-    // Integer width = arenaUpdate.arena.dims.get(0);
-    // Integer height = arenaUpdate.arena.dims.get(1);
-    String myUrl = "https://35.227.242.25.sslip.io";
-    PlayerState myState = arenaUpdate.arena.state.get(myUrl);
-
-    counter = counter < 10000 ? counter + 1 : 0;
-
-    if (myState == null) {
-        System.err.print("Got null for myState");
-        return "T";
+    public Application() throws Descriptors.DescriptorValidationException, IOException, InterruptedException {
+        writeCommittedStream = new WriteCommittedStream(projectId, datasetName, tableName);
     }
 
-    if (myState.wasHit) {
-        if (counter % 3 == 0) {
-            return "L";
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.initDirectFieldAccess();
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "Let the battle begin!";
+    }
+
+    @PostMapping("/**")
+    public String index(@RequestBody ArenaUpdate arenaUpdate) {
+        // Integer width = arenaUpdate.arena.dims.get(0);
+        // Integer height = arenaUpdate.arena.dims.get(1);
+        writeCommittedStream.sendArena(arenaUpdate.arena);
+        String myUrl = "https://35.227.242.25.sslip.io";
+        PlayerState myState = arenaUpdate.arena.state.get(myUrl);
+
+        counter = counter < 10000 ? counter + 1 : 0;
+
+        if (myState == null) {
+            System.err.print("Got null for myState");
+            return "T";
+        }
+
+        if (myState.wasHit) {
+            if (counter % 3 == 0) {
+                return "L";
+            } else {
+                return "F";
+            }
+        }
+
+        if (myState.score >= prevScore) {
+            prevScore = myState.score;
+            return "T";
         } else {
-            return "F";
+            prevScore = myState.score;
+        }
+
+        if (counter % 2 == 0) {
+            return "R";
+        } else {
+            return "T";
         }
     }
 
-    if (myState.score >= prevScore) {
-        prevScore = myState.score;
-        return "T";
-    } else {
-        prevScore = myState.score;
+    static class Self {
+        public String href;
     }
 
-    if (counter % 2 == 0) {
-        return "R";
-    } else {
-        return "T";
+    static class Links {
+        public Self self;
     }
-  }
 
-  private Integer prevScore = -100;
-  private Integer counter = 0;
+    static class PlayerState {
+        public Integer x;
+        public Integer y;
+        public String direction;
+        public Boolean wasHit;
+        public Integer score;
+    }
+
+    static class Arena {
+        public List<Integer> dims;
+        public Map<String, PlayerState> state;
+    }
+
+    static class ArenaUpdate {
+        public Links _links;
+        public Arena arena;
+    }
+
+    static class WriteCommittedStream {
+
+        final JsonStreamWriter jsonStreamWriter;
+
+        public WriteCommittedStream(String projectId, String datasetName, String tableName) throws IOException, Descriptors.DescriptorValidationException, InterruptedException {
+
+            try (BigQueryWriteClient client = BigQueryWriteClient.create()) {
+
+                WriteStream stream = WriteStream.newBuilder().setType(WriteStream.Type.COMMITTED).build();
+                TableName parentTable = TableName.of(projectId, datasetName, tableName);
+                CreateWriteStreamRequest createWriteStreamRequest =
+                        CreateWriteStreamRequest.newBuilder()
+                                .setParent(parentTable.toString())
+                                .setWriteStream(stream)
+                                .build();
+
+                WriteStream writeStream = client.createWriteStream(createWriteStreamRequest);
+
+                jsonStreamWriter = JsonStreamWriter.newBuilder(writeStream.getName(), writeStream.getTableSchema()).build();
+            }
+        }
+
+        public ApiFuture<AppendRowsResponse> sendArena(Arena arena) {
+            Instant now = Instant.now();
+            JSONArray jsonArray = new JSONArray();
+
+            arena.state.forEach((url, playerState) -> {
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("x", playerState.x);
+                jsonObject.put("y", playerState.y);
+                jsonObject.put("direction", playerState.direction);
+                jsonObject.put("wasHit", playerState.wasHit);
+                jsonObject.put("score", playerState.score);
+                jsonObject.put("player", url);
+                jsonObject.put("timestamp", now.getEpochSecond() * 1000 * 1000);
+                jsonArray.put(jsonObject);
+            });
+
+            return jsonStreamWriter.append(jsonArray);
+        }
+    }
 }
 
